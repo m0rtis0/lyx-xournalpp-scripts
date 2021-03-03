b54ac6da4f29afb06529e1aadaddd355d63b4f2b
m0rtis0 <65440078+m0rtis0@users.noreply.github.com>
2021-03-03 18:18:31 +0100

    Fixed paths

--- open_xournalpp.sh
+++ open_xournalpp.sh
@@ -2,11 +2,11 @@
 FPATH=$1/$2.xopp
 NPATH=$1/$2.svg
 if [ ! -f "$FPATH"]; then
-    cp ~/Documents/Schule/2021/General/template.xopp $FPATH
+    cp /home/YOURUSERNAME/path/to/template.xopp $FPATH
 fi
 xournalpp $FPATH
 xournalpp -i $NPATH $FPATH
 
 if [ ! -f "$FPATH"]; then
-    echo "LYXCMD:shell:inset-insert:graphics filename $NPATH" > ~/.lyx/lyxpipe.in
+    echo "LYXCMD:shell:inset-insert:graphics filename $NPATH" > /home/YOURUSERNAME/.lyxpipe.in
 fi
